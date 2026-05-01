28a54d4335a2ef96a200d21e7cdbce33fe20fb16
Fabricio Cesar <fabricioestevam46@gmail.com>
2026-05-01 20:54:15 -0300

    Tipos de dados JS - Number

---
+++ 04_tipos_de_dados_number/js/scripts.js
@@ -0,0 +1,19 @@
+var numero = 5;
+
+console.log(numero);
+console.log(typeof numero);
+
+var numeroDecimal = 5.5;
+
+console.log(numeroDecimal);
+console.log(typeof numeroDecimal);
+
+var numeroNegativo = -10;
+
+console.log(numeroNegativo);
+console.log(typeof numeroNegativo);
+
+var numeroCientifico = 1e6; // 1 x 10^6
+
+console.log(numeroCientifico);
+console.log(typeof numeroCientifico);
