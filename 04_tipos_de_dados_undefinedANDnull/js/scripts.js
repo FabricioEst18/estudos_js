9bf9669cc0082abc4c820b895bcf49481fabeb73
Fabricio Cesar <fabricioestevam46@gmail.com>
2026-05-01 21:27:27 -0300

    Tipos de dados em JS - null & undefined

---
+++ 04_tipos_de_dados_undefinedANDnull/js/scripts.js
@@ -0,0 +1,17 @@
+// null: representa a ausência intencional de um valor. É um valor atribuído a uma variável para indicar que ela não possui um valor válido ou que o valor é desconhecido.
+var nome = null;
+
+console.log(nome); // null
+
+nome = "João";
+
+console.log(nome); // João
+
+// undefined: representa a ausência de um valor ou a falta de definição de uma variável. É o valor padrão atribuído a uma variável que foi declarada, mas não inicializada.
+var idade;
+
+console.log(idade); // undefined
+
+idade = 30;
+
+console.log(idade); // 30
